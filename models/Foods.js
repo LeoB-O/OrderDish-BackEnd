8739781156fd09ca8107f6fab1099d510d469aeb
yangcrazy30 <yangcrazy30@hotmail.com>
2018-06-05 09:23:53 +0800

    a bug has solved

--- models/Foods.js
+++ models/Foods.js
@@ -2,7 +2,7 @@ const db = require("../db");
 
 module.exports = db.defineModel('foods', {
     Fname:db.STRING(50),
-    describleA:db.TEXT,
+    describeA:db.TEXT,
     image:db.TEXT,
     price:db.FLOAT,
     trueprice:db.FLOAT,
--- models/Orderfood.js
+++ models/Orderfood.js
@@ -1,5 +1,7 @@
 const db = require('../db');
 module.exports=db.defineModel('orderfood',{
+    // fid:{type:db.STRING(50),references:{model:'Foods',key:'id'}},
+    // orderid:{type:db.STRING(50),references:{model:'Orders',key:'id'}},
     fid:db.STRING(50),
     orderid:db.STRING(50)
 });
