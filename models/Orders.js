275893f1fbb5ec24aea8a5316c969bba756c3459
yangcrazy30 <yangcrazy30@hotmail.com>
2018-06-28 08:22:00 +0800

    addcart update

--- models/Orders.js
+++ models/Orders.js
@@ -4,8 +4,13 @@ module.exports=db.defineModel('ordert',{
     state:db.INTEGER,
     price:db.FLOAT,
     addressid:db.TEXT,
-    time:db.BIGINT,
-    extra:db.TEXT,
+    time:{type:db.BIGINT,allowNull:true},
+    extra:{type:db.TEXT,allowNull:true},
     Uid:db.STRING(50),
+<<<<<<< HEAD
+    disid:{type:db.STRING(50),allowNull:true},
+    shopid:db.STRING(50)
+=======
     disid:db.STRING(50)
+>>>>>>> origin/master
 });
